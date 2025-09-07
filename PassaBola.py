e4c8e44dcefb47639902fa899f390994c741ce1c
isabella marques <isabella>
2025-09-07 00:36:25 -0300

    ajustes e função de cadastro

--- PassaBola.py
+++ PassaBola.py
@@ -1,25 +1,4 @@
 # =========================================
-# Funções Básicas para validar dados inseridos pelos usuários
-# =========================================
-
-def verifica_numero(msg):
-    """Solicita que o usuário digite um número e valida a entrada."""
-    num = input(msg)
-    while not num.isnumeric():
-        print('Valor inválido. Tente novamente.')
-        num = input(msg)
-    return int(num)
-
-def forca_opcao(msg, lista_opcoes, msg_erro='Erro'):
-    """Força o usuário a escolher uma opção dentro da lista fornecida."""
-    opcoes = ', '.join(lista_opcoes)
-    escolha = input(f"{msg}\n{opcoes}\n-> ")
-    while escolha not in lista_opcoes:
-        print(msg_erro)
-        escolha = input(f"{msg}\n{opcoes}\n-> ")
-    return escolha
-
-# =========================================
 # Estruturas de dados do sistema
 # =========================================
 
@@ -30,22 +9,24 @@ usuarios = {
         "tipo": "comum",
         "nome": "Isabella",
         "sobrenome": "Marques",
+        "idade": 18,
         "email": "isa@gmail.com",
         "categoria": None,
         "nivel": None,
-        "amigos": [],
-        "favoritos": {"jogadoras": [], "times": []}
+        "amigos": ["maria456"],
+        "favoritos": {"jogadoras": ["jogadora_luiza"], "times": ["Flamengo"]}
     },
     "maria456": {
         "senha": "abcd",
         "tipo": "comum",
         "nome": "Maria",
         "sobrenome": "Silva",
+        "idade": 20,
         "email": "maria@gmail.com",
         "categoria": None,
         "nivel": None,
-        "amigos": [],
-        "favoritos": {"jogadoras": [], "times": []}
+        "amigos": ["isa123"],
+        "favoritos": {"jogadoras": ["jogadora_paula"], "times": ["Flamengo"]}
     },
     "jogadora_paula": {
         "senha": "paula123",
@@ -54,8 +35,10 @@ usuarios = {
         "categoria": None,
         "nome": "Paula",
         "sobrenome": "Alves",
+        "idade": 21,
         "email": "paula@gmail.com",
-        "amigos": [],
+        "rg": "21785046",
+        "amigos": ["jogadora_luiza","jogadora_ana","jogadora_gi"],
         "favoritos": {"jogadoras": [], "times": []}
     },
     "jogadora_luiza": {
@@ -65,8 +48,10 @@ usuarios = {
         "categoria": None,
         "nome": "Luiza",
         "sobrenome": "Carvalho",
+        "idade": 23,
         "email": "lu@gmail.com",
-        "amigos": [],
+        "rg": "34567891",
+        "amigos": ["jogadora_paula","jogadora_ana","jogadora_gi"],
         "favoritos": {"jogadoras": [], "times": []}
     },
     "jogadora_ana": {
@@ -76,19 +61,23 @@ usuarios = {
         "categoria": "Sub-17",
         "nome": "Ana",
         "sobrenome": "Santos",
+        "idade": 17,
         "email": "ana@gmail.com",
-        "amigos": [],
+        "rg": "93458651",
+        "amigos": ["jogadora_luiza","jogadora_paula","jogadora_gi"],
         "favoritos": {"jogadoras": [], "times": []}
     },
     "jogadora_gi": {
         "senha": "gi123",
         "tipo": "jogadora",
         "nivel": "amador",
-        "categoria": "Sub-20",
+        "categoria": "Adulta",
         "nome": "Giovana",
         "sobrenome": "Mendes",
+        "idade": 20,
         "email": "giovana@gmail.com",
-        "amigos": [],
+        "rg": "43217690",
+        "amigos": ["jogadora_luiza","jogadora_ana","jogadora_paula"],
         "favoritos": {"jogadoras": [], "times": []}
     },
     "admin01": {
@@ -152,4 +141,75 @@ campeonatos = [
     {"id": 1, "nome": "Copa Feminina 2025", "times": [1, 2], "jogos": [2]}
 ]
 
+# =========================================
+# Funções do código
+# =========================================
 
+
+# Solicita que o usuário digite um número e valida a entrada.
+
+def verifica_numero(msg):
+    num = input(msg)
+    while not num.isnumeric():
+        print('Valor inválido. Tente novamente.')
+        num = input(msg)
+    return int(num)
+
+# Força o usuário a escolher uma opção dentro da lista fornecida.
+
+def forca_opcao(msg, lista_opcoes, msg_erro='Erro'):
+    opcoes = ', '.join(lista_opcoes)
+    escolha = input(f"{msg}\n{opcoes}\n-> ")
+    while escolha not in lista_opcoes:
+        print(msg_erro)
+        escolha = input(f"{msg}\n{opcoes}\n-> ")
+    return escolha
+
+# Função de cadastro de usuário
+
+def cadastrar_usuario():
+    print("\n--- Cadastro de Usuário ---")
+    username = input("Escolha um nome de usuário: ")
+
+    # verifica se já existe
+    if username in usuarios:
+        print("Erro: nome de usuário já existe!")
+        return
+
+    senha = input("Digite a senha: ")
+    nome = input("Nome: ")
+    sobrenome = input("Sobrenome: ")
+    idade = verifica_numero("Digite a sua idade: ")
+    email = input("Email: ")
+
+    # tipo de usuário (comum ou jogadora ou admin)
+    tipo = forca_opcao("Escolha o tipo de usuário:", ["comum", "jogadora", "administrador"])
+
+    novo_usuario = {
+        "senha": senha,
+        "tipo": tipo,
+        "nome": nome,
+        "sobrenome": sobrenome,
+        "idade": idade,
+        "email": email
+    }
+
+    # Se for jogadora
+
+    if tipo == "jogadora":
+
+        rg = input("Digite o seu RG: ")
+
+        # categoria automática pela idade
+        if idade <= 15:
+            categoria = "Sub-15"
+        elif idade <= 17:
+            categoria = "Sub-17"
+        else:
+            categoria = "Adulta"
+
+        novo_usuario["rg"] = rg
+        novo_usuario["categoria"] = categoria
+
+    usuarios[username] = novo_usuario
+    print(f"Usuário {username} cadastrado com sucesso!")
